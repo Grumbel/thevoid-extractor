6ba7a64569a86bdc91cfb9e5d74f2c7e572977ab
Ingo Ruhnke <grumbel@gmail.com>
2022-10-29 12:25:10 +0200

    Add checkPhase to Nix build

--- flake.nix
+++ flake.nix
@@ -21,6 +21,24 @@
 
             src = ./.;
 
+            doCheck = true;
+
+            checkPhase = ''
+              runHook preCheck
+              flake8 --max-line-length 120 thevoid_extractor.py
+              mypy --strict thevoid_extractor.py
+              pylint thevoid_extractor.py
+              runHook postCheck
+            '';
+
+            checkInputs = with pythonPackages; [
+              flake8
+              mypy
+              pylint
+              types-setuptools
+              pip
+            ];
+
             propagatedBuildInputs = with pythonPackages; [
               setuptools
             ];
