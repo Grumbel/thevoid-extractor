f958070f460f2b0f2a4f23ae464b7baffe7c3916
Ingo Ruhnke <grumbel@gmail.com>
2023-05-26 13:07:08 +0200

    Update flake.nix to nixos-23.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "File extractor for the game The Void";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -21,8 +21,6 @@
 
             src = ./.;
 
-            doCheck = true;
-
             checkPhase = ''
               runHook preCheck
               flake8 --max-line-length 120 thevoid_extractor.py
@@ -31,7 +29,7 @@
               runHook postCheck
             '';
 
-            checkInputs = with pythonPackages; [
+            nativeCheckInputs = with pythonPackages; [
               flake8
               mypy
               pylint
