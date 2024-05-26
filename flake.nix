c3442de56ec7baa3e942f20ec3ac068a5283632f
Ingo Ruhnke <grumbel@gmail.com>
2024-05-26 17:10:56 +0200

    Update to nixos-24.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "File extractor for the game The Void";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
